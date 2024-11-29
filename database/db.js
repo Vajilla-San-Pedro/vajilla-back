5e6b139de8456f4ebd80b5a99b071c1c15936133
GonzaSalda <gonza.salda00@gmail.com>
2024-11-29 02:44:38 -0300

    Refactor database connection code

--- database/db.js
+++ database/db.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Cargar las variables de entorno desde .env
-const Product = require("../models/Product"); // Asegúrate de que el path sea correcto
+require("dotenv").config(); 
 
-// La URI de conexión que obtuviste de MongoDB Atlas
 const uri = process.env.MONGO_URI;
 
-// Función para conectar a la base de datos con Mongoose
 const connectDB = async () => {
   try {
-    // Conectar con la base de datos
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
-    process.exit(1); // Termina el proceso si la conexión falla
+    process.exit(1);
   }
 };
 
-// Exportar la función de conexión
 module.exports = connectDB;
