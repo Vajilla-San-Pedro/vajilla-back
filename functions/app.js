6b56e2aa46678aef9e9b9e587fca689c1d934460
GonzaSalda <gonza.salda00@gmail.com>
2024-11-29 15:48:53 -0300

    Add Netlify configuration and serverless deployment

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const connectDB = require('./database/db');
-const productRoutes = require('./router');   // Asegúrate de que este archivo tenga la ruta correcta
+const connectDB = require('../database/db');
+const productRoutes = require('../router');   // Asegúrate de que este archivo tenga la ruta correcta
 const app = express();
 const cors = require('cors'); // Importa el paquete cors
-
+import serverless from 'serverless-http';
 
 const port = process.env.PORT || 5001;
 
@@ -20,6 +20,9 @@ connectDB();
 
 // Usar el prefijo '/products' para las rutas de productos
 app.use('/products', productRoutes);
+
+app.use('netlify/functions/app', productRoutes);
+export const handler = serverless(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
