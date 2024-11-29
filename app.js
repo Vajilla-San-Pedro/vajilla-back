10923452163937a9de9e413f75fb8de11929fcbb
GonzaSalda <gonza.salda00@gmail.com>
2024-11-28 21:40:31 -0300

    Add .env file and update app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const corsOptions = {
-    origin: 'https://vajillaymanteleriazonaoeste.netlify.app',  // Ajusta con tu dominio exacto
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  };
-  
-  app.use(cors(corsOptions));
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use('/', require('./router'));
+    
 
-app.listen(5000, () =>{
-    console.log('Server is running on port 5000');
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
